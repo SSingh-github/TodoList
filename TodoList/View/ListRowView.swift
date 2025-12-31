5377cabd1d0f78d546f56fbf4449c40d577553c0
Sukhpreet Singh <ss0218308@gmail.com>
2025-12-31 15:09:16 +0530

    use the item model in the views

--- TodoList/View/ListRowView.swift
+++ TodoList/View/ListRowView.swift
@@ -7,16 +7,25 @@
 import SwiftUI
 
 struct ListRowView: View {
-    var title: String
+    var item: ItemModel
     var body: some View {
         
         HStack {
-            Image(systemName: "checkmark.circle")
-            Text(title)
+            Image(systemName: item.isCompleted ? "checkmark.circle" : "circle")
+                .foregroundColor(item.isCompleted ? .green : .red)
+            Text(item.title)
             Spacer()
         }
+        .font(.title2)
+        .padding(.vertical, 8)
     }
 }
-#Preview{
-    ListRowView(title: "This is preview")
+#Preview("first item", traits: .sizeThatFitsLayout){
+    let item1 = ItemModel(title: "This is first item", isCompleted: true)
+    ListRowView(item: item1)
+}
+
+#Preview("second item", traits: .sizeThatFitsLayout){
+    let item2 = ItemModel(title: "This is second item", isCompleted: false)
+    ListRowView(item: item2)
 }
--- TodoList/View/ListView.swift
+++ TodoList/View/ListView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ListView: View {
-    @State var items: [String] = [
-        "This is first item",
-        "This is second item"
+    @State var items: [ItemModel] = [
+        ItemModel(title: "This is the first item", isCompleted: false),
+        ItemModel(title: "This is the second item", isCompleted: true),
+        ItemModel(title: "This is the third item", isCompleted: false)
     ]
+    
     var body: some View {
         List {
-            ForEach(items, id: \.self) { item in
-                ListRowView(title: item)
+            ForEach(items) { item in
+                ListRowView(item: item)
             }
         }
         .listStyle(PlainListStyle())
