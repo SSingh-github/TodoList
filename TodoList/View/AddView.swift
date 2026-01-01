48b9e5301b271c9c3393f21f657cf5bde20fdc2f
Sukhpreet Singh <ss0218308@gmail.com>
2026-01-01 17:52:54 +0530

    use the viewmodel as the environment object in the app and use the view model in all the screens

--- TodoList/View/AddView.swift
+++ TodoList/View/AddView.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct AddView: View {
+    @EnvironmentObject var listViewModel: ListViewModel
+    @Environment(\.presentationMode) var presentationMode
     @State var textFieldText = ""
+    @State var showAlert: Bool = false
+    @State var alertTitle: String = ""
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -19,7 +24,7 @@ struct AddView: View {
                     .cornerRadius(10)
                 
                 Button {
-                    
+                    saveButtonPressed()
                 } label: {
                     Text("Save".uppercased())
                         .foregroundColor(.white)
@@ -34,6 +39,25 @@ struct AddView: View {
             .padding(14)
         }
         .navigationTitle("Add an item")
+        .alert(alertTitle, isPresented: $showAlert) {
+            
+        }
+    }
+    
+    func saveButtonPressed() {
+        if textIsAppropriate() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "The title should atleast 3 characters long!!!!"
+            showAlert = true
+            return false
+        }
+        return true
     }
 }
 
--- TodoList/View/ListView.swift
+++ TodoList/View/ListView.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 
 struct ListView: View {
-    @State var items: [ItemModel] = [
-        ItemModel(title: "This is the first item", isCompleted: false),
-        ItemModel(title: "This is the second item", isCompleted: true),
-        ItemModel(title: "This is the third item", isCompleted: false)
-    ]
+    
+    @EnvironmentObject var listViewModel: ListViewModel
     
     var body: some View {
         List {
-            ForEach(items) { item in
+            ForEach(listViewModel.items) { item in
                 ListRowView(item: item)
+                    .onTapGesture {
+                        listViewModel.updateItem(item: item)
+                    }
             }
+            .onDelete (perform: listViewModel.deleteItem)
+            .onMove (perform: listViewModel.moveItem)
         }
         .listStyle(PlainListStyle())
         .navigationTitle("Todo List")
